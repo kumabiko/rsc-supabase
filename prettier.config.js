a85a556830331258b9ec7540859b73b46d207e83
kumabiko <nousbear.dev@gmail.com>
2023-06-03 22:13:28 +0900

    📦 eslint prettier husky

---
+++ prettier.config.js
@@ -0,0 +1,24 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  //prettier
+  printWidth: 100,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'es5',
+  useTabs: false,
+  //@ianvs/prettier-plugin-sort-imports
+  plugins: ['@ianvs/prettier-plugin-sort-imports'],
+  importOrder: [
+    '^(react/(.*)$)|^(react$)',
+    '^(next/(.*)$)|^(next$)',
+    '',
+    '«THIRD_PARTY _MODULES>',
+    '^types$',
+    '^@/components/(.*)$',
+    '^@/app/(.*)$',
+    '',
+    '^[./]',
+  ],
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderTypeScriptVersion: '5.1.3',
+};
