32acdedaa49b8f0cf4b6fd0ffba4f7b77106b56f
kumabiko <nousbear.dev@gmail.com>
2023-06-04 18:12:05 +0900

    ✨ error Client Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
 import { Suspense } from 'react';
 
 import NotesList from '@/app/components/notes-list';
+import RefreshBtn from '@/app/components/refresh-btn';
+import Spinner from '@/app/components/spinner';
+import TimerCounter from '@/app/components/timer-counter';
 
 export default function Page() {
   return (
     <main>
       <div className="m-10 text-center">
         <p>Hello World</p>
-        <Suspense>
+        <Suspense fallback={<Spinner color="border-green-500" />}>
+          {/* @ts-expect-error Async Server Component */}
           <NotesList />
         </Suspense>
+        <TimerCounter />
+        <RefreshBtn />
       </div>
     </main>
   );
